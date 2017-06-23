0a881166a59b1dd906108ec34c993c97f3f1254e
Andrew Kostela <kostela1992@gmail.com>
2017-06-23 16:29:50 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// alert('Hello World');
+
+// document.write('Hello World!');
+
+// console.log('Hello World');
+
+
+var userName = prompt('Enter your name');
+
+document.write('Hello ' + userName);
+
+var userAge = Number(prompt('Enter your age'));
+
+document.write('<br>');
+document.write('Your age is ' + userAge);
+
+var newAge = sumThree(userAge, 1, 1); // userAge + 1;
+
+document.write('<br>');
+document.write('After the next year you will be ' + newAge);
+
+document.write('<br>');
+document.write('You are ' + checkAge(userAge));
+
+function sum(a, b) {
+    var result = a + b;
+    return result;
+}
+
+function sumThree(a, b, c) {
+    var result = a + b + c;
+    return result;
+}
+
+function checkAge(age) {
+    if (age < 35) {
+        return 'young';
+    } else {
+        return 'not young';
+    }
+}
+
+document.write(Math.sqrt(4));
